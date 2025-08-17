e479729d7907a9ee0033ca80333d304443261d13
Uday <2k22.csai.2213039@gmail.com>
2025-08-17 13:45:08 +0530

    updated Side Bar

--- src/components/app-sidebar.jsx
+++ src/components/app-sidebar.jsx
@@ -1,4 +1,4 @@
-import { Calendar, Home, Inbox, Search, Settings } from "lucide-react"
+import { Calendar, Home, Inbox, Pencil, Search, Settings } from "lucide-react"
 
 import {
   Sidebar,
@@ -15,28 +15,23 @@ import {
 const items = [
   {
     title: "Home",
-    url: "#",
+    url: "/",
     icon: Home,
   },
   {
-    title: "Inbox",
-    url: "#",
+    title: "Blogs",
+    url: "/blogs",
     icon: Inbox,
   },
   {
-    title: "Calendar",
-    url: "#",
-    icon: Calendar,
+    title: "Drafts",
+    url: "/drafts",
+    icon: Pencil,
   },
   {
     title: "Search",
-    url: "#",
+    url: "/search",
     icon: Search,
-  },
-  {
-    title: "Settings",
-    url: "#",
-    icon: Settings,
   },
 ]
 
